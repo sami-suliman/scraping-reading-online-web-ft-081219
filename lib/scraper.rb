6110fce66d23b0c37245a0cb80d831cbef0a09cf
sami suliman <sami.bco@gmail.com>
2019-09-18 21:41:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+base_url = 'https://www.stryker.com'
 
 
+    index_page =  base_url + '/us/en/portfolios/medical-surgical-equipment/infrastructure-and-room-design/ed.html/'
 
+    doc = Nokogiri::HTML(open(index_page))
+
+    doc.css('.product-item').each do |equip_css|
+
+       binding.pry
+
+    end
